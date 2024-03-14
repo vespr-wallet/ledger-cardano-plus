4ac8fef3bd5ec20e2b111886cc892969e3f4f9ce
Dinu Catalin <dinucatalin17@gmail.com>
2024-03-14 17:25:09 +0200

    formatting
    Co-Authored-By: Dinu Claudiu <81983738+claudiudinu98@users.noreply.github.com>

--- lib/src/models/spending_data_source.dart
+++ lib/src/models/spending_data_source.dart
@@ -5,10 +5,13 @@ part 'spending_data_source.freezed.dart';
 @freezed
 sealed class SpendingDataSource with _$SpendingDataSource {
   const SpendingDataSource._();
+
   const factory SpendingDataSource.none() = SpendingDataSourceNone;
+
   const factory SpendingDataSource.path({
     required List<int> path,
   }) = SpendingDataSourcePath;
+
   const factory SpendingDataSource.scriptHash({
     required String scriptHashHex,
   }) = SpendingDataSourceScriptHash;
--- lib/src/models/staking_data_source.dart
+++ lib/src/models/staking_data_source.dart
@@ -7,17 +7,21 @@ sealed class StakingDataSource with _$StakingDataSource {
   const StakingDataSource._();
 
   const factory StakingDataSource.none() = StakingDataSourceNone;
+
   const factory StakingDataSource.keyPath({
     required List<int> path,
   }) = StakingDataSourceKeyPath;
+
   const factory StakingDataSource.keyHash({
     required String keyHashHex,
   }) = StakingDataSourceKeyHash;
+
   const factory StakingDataSource.blockchainPointer({
     required int blockIndex,
     required int txIndex,
     required int certificateIndex,
   }) = StakingDataSourceBlockchainPointer;
+
   const factory StakingDataSource.scriptHash({
     required String scriptHashHex,
   }) = StakingDataSourceScriptHash;

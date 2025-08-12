d126981afb08b496742fe5fb0685d2e6f21ee9f4
Alexandru Dochioiu <alex@dochioiu.com>
2025-08-12 22:12:40 +0300

    [TEST] Added new test case for midnight signing

--- example/integration_test/sign_message_test_cases.dart
+++ example/integration_test/sign_message_test_cases.dart
@@ -288,4 +288,25 @@ final testsSignMessage = [
       signatureType: DataSignatureType.paylod,
     ),
   ),
+  SignMessageTestCase(
+    testName: 'msg13: Should correctly sign midnight claim with no hashing',
+    messageData: ParsedMessageData.keyHash(
+      messageHex:
+          '535441522037363736303136343920746f2061646472317138707668656868687875766561637676716e3936676d79717171633568717167797a6c6464777633677467643861306a6a346d34327236673876366d3234757074376c6c6c6e736c6d7a32376e643530796335726c7a636e6e3873636b647338352033316136626162353061383462383433396164636662373836626232303230663638303765366538666461363239623432343131306663376262316336623862',
+      signingPath: LedgerSigningPath.shelley(
+        account: 0,
+        role: ShelleyAddressRole.payment,
+        address: 1,
+      ), // str_to_path("1852'/1815'/0'/3/0"),
+      hashPayload: false,
+    ),
+    network: CardanoNetwork.mainnet(),
+    expected: SignedMessageData(
+      signatureHex:
+          '16acce5836bbf54d892dd8d694c53c50c569ecf013d18b666933fee86adb0530ab86ef06085053059bd7889d815ecec795260b092af82748a31bf43c8935820f',
+      signingPublicKeyHex: 'b3d5f4158f0c391ee2a28a2e285f218f3e895ff6ff59cb9369c64b03b5bab5eb',
+      addressFieldHex: '5a53103829a7382c2ab76111fb69f13e69d616824c62058e44f1a8b3',
+      signatureType: DataSignatureType.paylod,
+    ),
+  ),
 ];
